0ce7b8925fd6195c621bfa28560cc44030938bfe
vladimirpekarski <vladimir.pekarski@gmail.com>
2014-12-28 17:40:49 +0300

    correction SimpleExampleTestClass Adding Log4j

--- src/test/java/SimpleExampleTest.java
+++ src/test/java/SimpleExampleTest.java
@@ -1,21 +1,35 @@
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class SimpleExampleTest {
+    private static final Logger LOG = Logger.getLogger(
+            SimpleExampleTest.class);
     private static final String FIRST_SITE = "http://www.tut.by";
     private static final String SECOND_SITE = "http://www.habrahabr.ru/";
     private WebDriver driver;
+
+    @BeforeTest
+    public void initLogger() {
+        PropertyConfigurator.configure("./src/log4j.properties");
+    }
 
     @BeforeMethod
     public void setup() throws InterruptedException {
         driver = new InternetExplorerDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+
         driver.get(FIRST_SITE);
-        Thread.sleep(3000);
+        LOG.info("setup finished");
     }
 
     @AfterMethod
